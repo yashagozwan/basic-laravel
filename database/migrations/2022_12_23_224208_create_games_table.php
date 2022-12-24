999bbd0d2208781bda07bd9e7313721331c01f65
Yasha Gozwan Shuhada <yashagozwan13@gmail.com>
2022-12-24 14:46:55 +0700

    games table migration

---
+++ database/migrations/2022_12_23_224208_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('hours');
+            $table->string('author');
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
